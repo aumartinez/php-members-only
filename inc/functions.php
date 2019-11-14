433541baf707cefc9f4b52c490aa560ac2e0a231
Augusto Martinez <acm_asd@hotmail.com>
2019-11-14 08:32:42 -0600

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -5,4 +5,8 @@ session_start();
 require_once("inc/dbkey.php");
 require_once("inc/validation.php");
 
+function  randomStr($length) {
+  return substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 0, $length);
+}
+
 ?>
