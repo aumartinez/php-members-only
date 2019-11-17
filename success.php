2e02892be1fbe5cd1adf51e8602d7b1a4fa9001a
Augusto Martinez <acm_asd@hotmail.com>
2019-11-17 16:02:34 -0600

    Update success.php

--- success.php
+++ success.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Setting the Table</title>
+    <title>Thank you</title>
     
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
@@ -41,7 +41,6 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/startup.js"></script>
+    <script src="js/bootstrap.min.js"></script>    
   </body>
 </html>
