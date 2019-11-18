5a47bda4e95215f683df12b3523c671b867fbecc
Augusto Martinez <acm_asd@hotmail.com>
2019-11-18 15:28:56 -0600

    Create class-user.php

---
+++ inc/class-user.php
@@ -0,0 +1,111 @@
+<?php
+
+class User {
+  public $id;
+  public $email;
+  public $firstName;
+  public $lastName;
+  public $address;
+  public $city;
+  public $state;
+  public $zip;
+  public $phone;
+  public $phoneType;
+  public $isLoggedIn = false;
+  
+  function __construct() {
+    if (session_id() == "") {
+      session_start();
+    }
+    
+    if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"] == true) {
+      $this->initUser();
+    }
+  }//End __construct
+  
+  public function authenticate($user, $pass) {
+    $conx = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+    if (!$conx) {
+      error_log("MySQL connect error: ".mysqli_connect_error());
+      return false;
+    }
+    
+    $regUser = mysqli_real_escape_string($conx, $user);
+    $password = mysqli_real_escape_string($conx, $pass);
+    
+    $sql = "SELECT *
+            FROM customer
+            WHERE email = '{$regUser}'";
+    
+    $query = mysqli_query($conx, $sql);
+    if (!$query) {
+      error_log("Cannot reach the account for {$user}");
+      return false;
+    }
+    
+    $result = mysqli_fetch_assoc($query);
+    $salt = $result["salt"];
+    $crypted = crypt($password, $salt);
+    $crypted = substr($crypted,strlen($salt));
+    
+    if ($crypted != $result["password"]) {
+      error_log("Password for {$user} don't match");
+      return false;
+    }
+    
+    $this->id = $result["id"];
+    $this->email = $result["email"];
+    $this->firstName = $result["first_name"];
+    $this->lastName = $result["last_name"];
+    $this->address = $result["street"];
+    $this->city = $result["city"];
+    $this->state = $result["state"];
+    $this->phone = $result["phone"];
+    $this->phoneType = $result["phone_type"];
+    $this->isLoggedIn = true;
+    
+    $this->_setSession();
+    
+    return true;
+  }// End authenticate
+  
+  private function _setSession() {
+    if (session_id() == "") {
+      session_start();
+    }
+    
+    $_SESSION["id"] = $this->id;
+    $_SESSION["email"] = $this->email;
+    $_SESSION["firstName"] = $this->firstName;
+    $_SESSION["lastName"] = $this->lastName;
+    $_SESSION["address"] = $this->address;
+    $_SESSION["city"] = $this->city;
+    $_SESSION["zip"] = $this->zip;
+    $_SESSION["state"] = $this->state;
+    $_SESSION["phone"] = $this->phone;
+    $_SESSION["phoneType"] = $this->phoneType;
+    $_SESSION["isLoggedIn"] = $this->isLoggedIn;
+    
+  }//End _setSession
+  
+  private function _initUser() {
+    if (session_id() == "") {
+      session_start();
+    }
+    
+    $this->id = $_SESSION["id"];
+    $this->email = $_SESSION["email"];
+    $this->firstName = $_SESSION["firstName"];
+    $this->lastName = $_SESSION["lastName"];
+    $this->address = $_SESSION["address"];
+    $this->city = $_SESSION["city"];
+    $this->zip = $_SESSION["zip"];
+    $this->state = $_SESSION["state"];
+    $this->phone = $_SESSION["phone"];
+    $this->phoneType = $_SESSION["phoneType"];
+    $this->isLoggedIn = $_SESSION["isLoggedIn"];
+    
+  }//End initUser
+}
+
+?>
