bc839406fb4677a4431f064f780726db03172892
Augusto Martinez <acm_asd@hotmail.com>
2019-11-18 16:13:28 -0600

    Create login-process.php

---
+++ login-process.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("inc/functions.php");
+
+if (!isset($_POST["submitForm"])) {
+  header("Location: login.php");
+  exit();
+}
+
+$_SESSION["submitForm"] = true;
+
+
+if (isset($_SESSION["error"])) {  
+  unset($_SESSION["error"]);
+}
+
+$_SESSION["error"] = array();
+
+$required = array(
+            "email",
+            "password"
+            );
+
+//Check required
+foreach ($required as $value) {
+  if (!(isset($_POST[$value]) || $_POST[$value] == "")) {
+    $_SESSION["error"][] = $value." is required";
+  }
+}
+
+//Validate email address
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+  $_SESSION["error"][] = "Email address is invalid.";
+}
+
+if (count($_SESSION["error"]) > 0) {
+  header("Location: login.php");
+}
+else {
+  $user = new User();
+  
+  if ($user->authenticate($_POST["email"], $POST["password"])) {
+    unset($_SESSION["formSubmit"]);
+    header("Location: index.php");
+    exit();
+  }
+  else {
+    $_SESSION["error"][] = "There was a problem with your user or password.";
+    header("Location: login.php");
+    exit();
+  }
+}
+
+?>
