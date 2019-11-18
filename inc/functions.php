f0e67432bc0da0770d937e84c991b3c8f7b4e409
Augusto Martinez <acm_asd@hotmail.com>
2019-11-18 15:33:39 -0600

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -4,6 +4,7 @@ session_start();
 
 require_once("inc/dbkey.php");
 require_once("inc/validation.php");
+require_once("inc/class-user.php");
 
 function randomStr($length) {  
   return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
