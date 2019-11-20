f118d8a9fc6bc676e5bf8dce3f7e04e225796b21
Augusto Martinez <acm_asd@hotmail.com>
2019-11-20 13:38:25 -0600

    Update class-user.php

--- inc/class-user.php
+++ inc/class-user.php
@@ -111,6 +111,150 @@ class User {
     $this->isLoggedIn = $_SESSION["isLoggedIn"];
     
   }//End initUser
+  
+  public function logout() {
+    $this->isLoggedIn = false;
+    
+    if (session_id() == "") {
+      session_start();
+    }
+    
+    $_SESSION["isLoggedIn"] = false;
+    
+    foreach($_SESSION as $key => $value) {
+      $_SESSION[$key] = "";
+      unset($_SESSION[$key]);
+    }
+    
+    $_SESSION = array();
+    
+    if (ini_get("seesion.use_cookies")) {
+      $cookieParameters = session_get_cookie_params();
+      setcookie(session_name(), "", time() - 28800, $cookieParameters["path"], $cookieParameters["domain"], $cookieParameters["secure"], $cookieParameters["httponly"]);
+    }
+    
+    session_destroy();
+  }
+  
+  public function emailPass($username) {
+    $conx = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+    if (!$conx) {
+      error_log("MySQL connect error: ".mysqli_connect_error());
+      return false;
+    }
+    
+    $user = mysqli_real_escape_string($username);
+    $sql = "SELECT id, email
+            FROM customer
+            WHERE email = '{$user}'";
+            
+    $query = mysqli_query($conx, $sql);
+    
+    if (!$query) {
+      $_SESSION["error"][] = "MySQL error: ".mysqli_errno();
+      mysqli_close($conx);
+      return false;
+    }
+    
+    $result = mysqli_fetch_row($query);
+    
+    if ($result == 0) {
+      $_SESSION["error"][] = "User with {$user} email not found."
+      mysqli_free_result($query);
+      mysqli_close($conx);
+      return false;
+    }
+    
+    $result = mysqli_fetch_assoc($query);
+    $id = $result["id"];
+    
+    $hash = uniqid("", TRUE);
+    $hash = md5($hash);
+    $hash = mysqli_real_escape_string($conx, $hash);
+    
+    $sql = "INSERT INTO resetPassword (
+              email_id,
+              pass_key,
+              create_date,
+              status
+            )
+            VALUES (
+              '{$id}',
+              '{$hash}',
+              NOW(),
+              'A'
+            )
+          ";
+    
+    $insert = mysqli_query($conx, $sql);
+    
+    if (!$insert) {
+      error_log("Couldn't insert data for ".$id);
+      $_SESSION["error"][] = "MySQL error: ".mysqli_errno();
+      mysqli_close($conx);
+      return false;
+    }
+    
+    $urlHash = urlencode($hash);
+    
+    $serverUrl = isset($_SERVER["HTTPS"]) ? "https://" : "http://";
+    $serverUrl .= $_SERVER["SEVER_NAME"];
+    
+    $str = $_SERVER["PHP_SELF"];
+    $arr = explode("/", $str);
+    $str = array();
+    
+    for ($i = 0; $i < count($arr); $i++) {
+      if ($i == (count($arr) - 1)) {
+        break;
+      }
+      array_push($str, $arr[$i]);      
+    }
+    
+    $str = join("/", $str);
+    $str = substr($str, 1);
+    
+    $serverUrl .= $str; //Script path
+    $serverUrl .= "/reset.php";
+    
+    $emailbody = '
+      <!doctype html>
+      <html>
+        <head>
+          <title>Email alert</title>
+        </head>
+        <body>
+          <div style="font-family: Arial, sans-serif; margin: 60px auto; width: 600px">
+            <h3 style="text-align: center">
+              Please click on the link to reset your password
+            </h3>
+            
+            <hr />
+            
+            <p>
+              Click <a href="'.$serverUrl.'">here</a> to open link to reset password page.
+            </p>
+          </div>
+        </body>
+      </html>
+    ';
+    
+    $to = $result["email"];
+    $subject = "Passwrod reset requested";
+    $txt = $emailbody;
+    $headers = array(
+                "MIME-Version: 1.0",
+                "Content-type:text/html;charset=UTF-8",
+                "From: no-reply@company.com",
+                "Reply-To: no-reply@company.com",
+                "X-Mailer: PHP/".PHP_VERSION
+                );
+                
+    $headers = implode("\r\n", $headers);
+    $sendemail = mail($to, $subject, $txt, $headers);
+  }//End emailPass
+  
+  
 }//End class User
 
 ?>
