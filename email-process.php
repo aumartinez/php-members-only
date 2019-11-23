b0fe89868d06a0c931a28077e52eed4e9a5d7c5b
Augusto Martinez <acm_asd@hotmail.com>
2019-11-23 07:24:30 -0600

    Create email-process.php

---
+++ email-process.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once("inc/functions.php");
+
+if (!isset($_POST["submitForm"])) {
+  header("Location: recovery.php");
+  exit();
+}
+
+$_SESSION["submitForm"] = true;
+
+
+if (isset($_SESSION["error"])) {  
+  unset($_SESSION["error"]);
+}
+
+$_SESSION["error"] = array();
+
+$required = array(
+            "email"
+            );
+
+//Check required
+foreach ($required as $value) {
+  if (!(isset($_POST[$value])) || $_POST[$value] == "") {
+    $_SESSION["error"][] = $value." is required";
+  }
+}
+
+//Validate email address
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+  $_SESSION["error"][] = "Email address is invalid.";
+}
+
+if (count($_SESSION["error"]) > 0) {
+  header("Location: recovery.php");
+  exit();
+}
+else {
+  $user = new User();
+  
+  if ($user->emailPass($_POST["email"])) {
+    unset($_SESSION["submitForm"]);
+    header("Location: email-success.php");
+    exit();
+  }
+  else {    
+    $_SESSION["error"][] = "Email address couldn't be located";
+    print_r($_SESSION["error"]);
+    //header("Location: recovery.php");
+    exit();
+  }
+}
+
+?>
